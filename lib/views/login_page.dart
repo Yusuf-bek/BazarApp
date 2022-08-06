2017e0043ce70e0e15bb4636ee8274758d6d4a37
Safarmurod <safarmurodmusulmonov@gmail.com>
2022-08-06 15:41:21 +0500

    login page change

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,8 +1,15 @@
 import 'package:bazarapp/core/components/size_config.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isNotVisible = true;
 
   @override
   Widget build(BuildContext context) {
@@ -10,131 +17,143 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  "assets/login_page.gif",
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: getConfigWidth(30),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      "assets/login_page.gif",
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            "Login",
-                            style: TextStyle(
-                              fontSize: 27,
-                              fontWeight: FontWeight.bold,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: getConfigWidth(30),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            color: Colors.white,
+                            width: double.infinity,
+                            height: MediaQuery.of(context).size.height * 0.16,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    const Text(
+                                      "@",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    Expanded(
+                                      child: SizedBox(
+                                        child: TextFormField(
+                                          decoration: const InputDecoration(
+                                            hintText: "login",
+                                            hintStyle: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                          keyboardType:
+                                              TextInputType.emailAddress,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.lock_outline_rounded,
+                                      color: Colors.grey,
+                                    ),
+                                    const SizedBox(
+                                      width: 15,
+                                    ),
+                                    Expanded(
+                                      child: SizedBox(
+                                        child: TextFormField(
+                                          decoration: const InputDecoration(
+                                            hintText: "parol",
+                                            hintStyle: TextStyle(
+                                              color: Colors.grey,
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                          keyboardType:
+                                              TextInputType.visiblePassword,
+                                          obscureText: _isNotVisible,
+                                        ),
+                                      ),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _isNotVisible = !_isNotVisible;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        _isNotVisible
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
+                          loginButton(context, onTap: () {}),
                         ],
                       ),
-                      Container(
-                        color: Colors.white,
-                        width: double.infinity,
-                        height: MediaQuery.of(context).size.height * 0.16,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const Text(
-                                  "@",
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Expanded(
-                                  child: SizedBox(
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                        hintText: "Email ID",
-                                        hintStyle: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                      keyboardType: TextInputType.emailAddress,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.lock_outline_rounded,
-                                  color: Colors.grey,
-                                ),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Expanded(
-                                  child: SizedBox(
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                        hintText: "Password",
-                                        hintStyle: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                      keyboardType:
-                                          TextInputType.visiblePassword,
-                                      obscureText: true,
-                                    ),
-                                  ),
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.visibility,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      loginButton(context, onTap: () {}),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -152,7 +171,7 @@ class LoginPage extends StatelessWidget {
         ),
         child: const Center(
           child: Text(
-            "Login",
+            "Kirish",
             style: TextStyle(
               color: Colors.white,
               fontSize: 15,
